88d97fde881e578f76c885f4fb5ae616be2b8bce
Kirkusik <kirkusik@i.ua>
2019-07-01 16:10:46 +0300

    m2

--- src/components/add-reviews/add-reviews.jsx
+++ src/components/add-reviews/add-reviews.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const AddReviews = () => {
+const AddReviews = ({onChangeRating, onChangeText, activeSubmit, onSubmit}) => {
+
   return <React.Fragment>
-    <form className="reviews__form form" action="#" method="post">
+    <form
+      onSubmit={(evt) => {
+        evt.preventDefault();
+        onSubmit();
+        evt.target.reset();
+      }}
+      className="reviews__form form"
+      action="#"
+      method="post">
       <label className="reviews__label form__label" htmlFor="review">Your review</label>
-      <div className="reviews__rating-form form__rating">
+      <div
+        onChange={(evt) => {
+          onChangeRating(evt.target.value);
+        }}
+        className="reviews__rating-form form__rating">
         <input className="form__rating-input visually-hidden" name="rating" defaultValue={5} id="5-stars" type="radio" />
         <label htmlFor="5-stars" className="reviews__rating-label form__rating-label" title="perfect">
           <svg className="form__star-image" width={37} height={33}>
@@ -36,15 +50,30 @@ const AddReviews = () => {
           </svg>
         </label>
       </div>
-      <textarea className="reviews__textarea form__textarea" id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved" defaultValue={``} />
+      <textarea
+        onChange={(evt) => {
+          onChangeText(evt.target.value);
+        }}
+        className="reviews__textarea form__textarea"
+        id="review"
+        name="review"
+        placeholder="Tell how was your stay, what you like and what can be improved"
+        defaultValue={``} />
       <div className="reviews__button-wrapper">
         <p className="reviews__help">
                     To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
         </p>
-        <button className="reviews__submit form__submit button" type="submit" disabled>Submit</button>
+        <button className="reviews__submit form__submit button" type="submit" disabled={activeSubmit ? false : true}>Submit</button>
       </div>
     </form>
   </React.Fragment>;
+};
+
+AddReviews.propTypes = {
+  onChangeRating: PropTypes.func,
+  onChangeText: PropTypes.func,
+  activeSubmit: PropTypes.bool,
+  onSubmit: PropTypes.func
 };
 
 export default AddReviews;
